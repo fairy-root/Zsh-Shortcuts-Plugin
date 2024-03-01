c6ea86ed45a87745e3a21d7122b0a6d3e5ff482d
FairyRoot <48371573+fairy-root@users.noreply.github.com>
2024-03-02 00:40:43 +0100

    Delete utils directory

--- utils/logging_reporting.zsh
+++
@@ -1,5 +0,0 @@
-# In utils/logging_reporting.zsh
-function log_operation() {
-    local operation="$1"
-    echo "$(date "+%Y%m%d%H%M%S") - $operation" >> "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/shortcuts/operations.log"
-}
